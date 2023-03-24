af97f04e65b319749642033d65c44d41bd0203ca
Cristian <105856280+cristian9751@users.noreply.github.com>
2023-03-24 21:56:14 +0100

    Eliminado app.java y creado platos java. Tambien se ha modificado el … (#10)
    * Eliminado app.java y creado platos java. Tambien se ha modificado el workflow de maven
    
    * Modificado maven.yaml
    
    * Modificado maven.yaml 2
    
    * Modificado maven.yaml 3
    
    * Otro cambio
    
    * Otro cambio
    
    * Otro cambio
    
    * Haciendo pruebas

---
+++ src/main/java/com/restaurante/platos.java
@@ -0,0 +1,12 @@
+package com.restaurante;
+
+/**
+ * @author cristian
+ * @project RESTAURANTE-Samuel-y-Cristian- - com.restaurante
+ * @created 24/3/23
+ */
+public class platos {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
