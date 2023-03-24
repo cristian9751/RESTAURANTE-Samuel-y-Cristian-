a42e22b6cdf3f18f16d7a8d07876e42cdb366460
cristian97511 <cristian975112@gmail.com>
2023-03-24 20:57:25 +0100

    Otro cambio

--- src/main/java/com/restaurante/platos.java
+++ src/main/java/com/restaurante/platos.java
@@ -2,7 +2,7 @@ package com.restaurante;
 
 /**
  * @author cristian
- * @project RESTAURANTE-Samuel-y-Cristian- - com.restaurante
+ * @project RESTAURANTE-Samuel-y-Cristian- - com.restaurante 
  * @created 24/3/23
  */
 public class platos {
