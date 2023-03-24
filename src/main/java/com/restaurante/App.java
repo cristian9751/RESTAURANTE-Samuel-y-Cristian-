de64128f820da056d5cbc551291a1232d262703d
cristian97511 <cristian975112@gmail.com>
2023-03-24 23:31:14 +0100

    Cambios para repositorio preparado

--- src/main/java/com/restaurante/App.java
+++ src/main/java/com/restaurante/App.java
@@ -5,8 +5,5 @@ package com.restaurante;
  * @project RESTAURANTE-Samuel-y-Cristian- - com.restaurante
  * @created 24/3/23
  */
-public class platos {
-    public static void main(String[] args) {
-        System.out.println();
-    }
+public class App {
 }
