a324a5e52d8cc600d612a383f2c7302b07d11c69
cristian97511 <cristian975112@gmail.com>
2023-03-25 14:13:41 +0100

    Clase ingrediente creada completamente. Falta hacer el testeo

--- src/main/java/com/restaurante/ingrediente.java
+++ src/main/java/com/restaurante/ingrediente.java
@@ -6,4 +6,51 @@ package com.restaurante;
  * @created 25/3/23
  */
 public class ingrediente {
+    private String nombre;
+    private String tipo;
+    private int cantidad;
+
+    public ingrediente(String nombre, String tipo, int cantidad) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        if(cantidad >= 0) {
+            this.cantidad = cantidad;
+        } else {
+            System.out.println("Los ingredientes deben de tener una cantidad mayor a 0");
+        }
+    }
+
+    public void AumentarCantidad(int cantidad) {
+        this.setCantidad(this.cantidad + cantidad);
+    }
+
+
+    public void ReducirCantidad(int cantidad) {
+        this.setCantidad(this.cantidad - cantidad);
+    }
+
 }
