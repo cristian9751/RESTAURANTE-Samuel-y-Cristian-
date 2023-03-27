816532c0efc71a468643936d1abdcbd11662e753
cristian9751 <cristian975112@gmail.com>
2023-03-27 11:21:57 +0200

    Terminado utilities

--- src/main/java/com/restaurante/utilidades.java
+++ src/main/java/com/restaurante/utilidades.java
@@ -4,4 +4,16 @@ import java.util.Scanner;
 
 public class utilidades {
     public static Scanner tcl = new Scanner(System.in);
+
+    public static String PedirString(String txt) {
+        System.out.println(txt);
+        return tcl.nextLine();
+    }
+
+    public static int PideEntero(String txt) {
+        System.out.println(txt);
+        return tcl.nextInt();
+    }
+
+
 }
